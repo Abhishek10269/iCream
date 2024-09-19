d8c54ba95acf9cc934fc8e6b47449b5a4a1ef55d
Abhishek kumar <7011042290abhi@gmail.com>
2024-09-20 02:17:10 +0530

    completed resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -5,36 +5,41 @@ import { SiTailwindcss, SiMongodb, SiExpress, SiNextdotjs } from "react-icons/si
 
 // about data
 
-const about = [
-    {
-        fieldName: "Name",
-        fieldValue: "Abhishek kumar"
-    },
-    {
-        fieldName: "Phone",
-        fieldValue: "+917678574104"
-    },
-    {
-        fieldName: "Email",
-        fieldValue: "shakeabhi403@gmail.com"
-    },
-    {
-        fieldName: "Experience",
-        fieldValue: "2+ Year"
-    },
-    {
-        fieldName: "Nationality",
-        fieldValue: "Indian"
-    },
-    {
-        fieldName: "Freelance",
-        fieldValue: "Available"
-    },
-    {
-        fieldName: "Languages",
-        fieldValue: "English,Hindi"
-    },
-];
+const about = {
+    title: "About Me",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quisquam itaque doloremque, laudantium ea molestias!",
+    info: [
+        {
+            fieldName: "Name",
+            fieldValue: "Abhishek kumar"
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "+917678574104"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "2+ Year"
+        },
+        {
+            fieldName: "Nationality",
+            fieldValue: "Indian"
+        },
+        {
+            fieldName: "Freelance",
+            fieldValue: "Available"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "shakeabhi403@gmail.com"
+        },
+        
+        {
+            fieldName: "Languages",
+            fieldValue: "English,Hindi"
+        },
+    ],
+};
 
 const experience = {
     icon: "/asset/resume/badge.svg",
@@ -174,41 +179,112 @@ const Resume = () => {
                         <TabsContent value="experience" className="w-full">
                             <div className="flex flex-col gap-[30px] text-center xl:text-left">
                                 <h3 className="text-4xl font-bold">{experience.title}</h3>
-                                    <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                                        {experience.description}
-                                    </p>
-                                    <ScrollArea className="h-[400px]">
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                                    {experience.description}
+                                </p>
+                                <ScrollArea className="h-[400px]">
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                                    {experience.items.map((item,index)=>{
-                                    return(
-                                    <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl
+                                        {experience.items.map((item, index) => {
+                                            return (
+                                                <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl
                                     flex flex-col justify-center items-center lg:items-start gap-1">
-                                    <span className="text-accent">{item.duration}</span>
-                                    <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
-                                    <div>
-                                        {/* {dot} */}
-                                    <span></span>
-                                    <p>{item.company}</p>
-                                    </div>
-                                    
-                                    </li>
-                                    );
-                                    })}
+                                                    <span className="text-accent">{item.duration}</span>
+                                                    <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                                                    <div className="flex items-center gap-3">
+                                                        {/* {dot} */}
+                                                        <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                                                        <p>{item.company}</p>
+                                                    </div>
+
+                                                </li>
+                                            );
+                                        })}
                                     </ul>
 
-                                    </ScrollArea>
-                                
+                                </ScrollArea>
+
                             </div>
                         </TabsContent>
                         {/* education */}
                         <TabsContent value="education" className="w-full">
-                            education
+                            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                <h3 className="text-4xl font-bold">{education.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                                    {education.description}
+                                </p>
+                                <ScrollArea className="h-[400px]">
+                                    <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                                        {education.items.map((item, index) => {
+                                            return (
+                                                <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl
+                                    flex flex-col justify-center items-center lg:items-start gap-1">
+                                                    <span className="text-accent">{item.duration}</span>
+                                                    <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                                                    <div className="flex items-center gap-3">
+                                                        {/* {dot} */}
+                                                        <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                                                        <p>{item.institution}</p>
+                                                    </div>
+
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+
+                                </ScrollArea>
+
+                            </div>
                         </TabsContent>
-                        <TabsContent value="skills" className="w-full">
-                            skills
+                        {/* {skills} */}
+                        <TabsContent value="skills" className="w-full h-full">
+                            <div className="flex flex-col gap-[30px]">
+                                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                    <h3 className="text-4xl font-bold">{skill.title}</h3>
+                                    <p className="max-w-[600px] text/white/60 mx-auto xl:mx-0">{skill.description}</p>
+                                </div>
+                                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-4">
+                                    {skill.items.map((item, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <TooltipProvider delayDuration={100}>
+                                                    <Tooltip>
+                                                        <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl 
+                                                        flex justify-center items-center group">
+                                                            <div className="text-6xl group-hover:text-accent transition-all
+                                                             duration-300">{item.icon}</div>
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            <p>{item.name}</p>
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                </TooltipProvider>
+                                            </li>
+
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                                    {/* {about} */}
                         </TabsContent>
                         <TabsContent value="about" className="w-full">
-                            about
+                            <div className="flex flex-col gap-[30px]">
+                                <h3 className="text-4xl font-bold">{about.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0"> {about.description}</p>
+                                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                                    {about.info.map((item,index)=>{
+                                        return(
+                                            <li key={index} 
+                                            className="flex items-center justify-center xl:justify-start gap-4"
+                                            >
+                                                <span className="text-white/60">{item.fieldName}</span>
+                                                <span className="text-xl">{item.fieldValue}</span>
+                                
+                                            </li>
+
+                                        );
+                                    })}
+                                </ul>
+                            </div>
                         </TabsContent>
                     </div>
                 </Tabs>
