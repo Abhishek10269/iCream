a565200b4261b596c0f4caab1c68bf562c9dc0ee
Abhishek kumar <7011042290abhi@gmail.com>
2024-09-27 20:16:48 +0530

    updated project card

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,12 +2,13 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import {BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
@@ -15,10 +16,10 @@ import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 const projects = [
     {
         num: "01",
-        title: "Frontend Project ",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.",
+        title: "Swift Wallet ",
+        description: "Your wallet app for a risk-free, fun financial adventure!",
         stack: ["Html 5", "Css 3", "Javascript"],
-        images: ["/assets/2.jpg", "/assets/1.webp"], // Add more image paths as needed
+        images: ["/assets/wallet1.png", "/assets/wallet2.png"],
         live: "https://Frontend Project-demo.com",
         github: "https://github.com/username/Frontend Project"
     },
@@ -27,7 +28,7 @@ const projects = [
         title: "Frontend Project",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.",
         stack: ["Html 5", "Css 3", "Javascript"],
-        images: ["/assets/2.jpg", "/assets/3.webp"], // Add more image paths as needed
+        images: ["/assets/2.jpg", "/assets/3.webp"],
         live: "https://Frontend Project-demo.com",
         github: "https://github.com/username/Frontend Project"
     },
@@ -36,13 +37,10 @@ const projects = [
         title: "Frontend Project",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque consequat, faucibus et, et.",
         stack: ["Html 5", "Css 3", "Javascript"],
-        images: ["/assets/2.jpg", "/assets/1.webp"], // Add more image paths as needed
+        images: ["/assets/2.jpg", "/assets/1.webp"],
         live: "https://Frontend Project-demo.com",
         github: "https://github.com/username/Frontend Project"
     }
-
-
-    // Add more projects here...
 ];
 
 const ProjectCard = ({ project }) => (
@@ -51,16 +49,13 @@ const ProjectCard = ({ project }) => (
         animate={{ opacity: 1, y: 0 }}
         whileHover={{ scale: 1.02 }}
         transition={{ duration: 0.5 }}
-        className="mb-10 bg-[#27272c] rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:shadow-2xl"
+        className="mb-10 bg-[#27272c] rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:shadow-2xl flex flex-col items-center"
     >
-        <div className="p-6">
-            <div className="flex justify-between items-center mb-4">
-                <span className="text-3xl  font-extrabold text-outline
-                            text-transparent">{project.num}</span>
-            </div>
+        <div className="p-6 text-center w-full">
+            <span className="text-3xl font-extrabold text-outline text-transparent inline-block mb-4">{project.num}</span>
             <h3 className="text-2xl font-bold mb-3 text-white">{project.title}</h3>
             <p className="text-sm text-gray-400 mb-4">{project.description}</p>
-            <div className="flex flex-wrap gap-2 mb-2">
+            <div className="flex flex-wrap gap-2 mb-4 justify-center">
                 {project.stack.map((tech, index) => (
                     <span
                         key={index}
@@ -72,51 +67,53 @@ const ProjectCard = ({ project }) => (
             </div>
         </div>
 
-        <Swiper
-            modules={[Navigation, Pagination]}
-            navigation
-            pagination={{ clickable: true }}
-            className="mySwiper"
-        >
-            {project.images.map((image, index) => (
-                <SwiperSlide key={index}>
-                    <Image
-                        src={image}
-                        alt={`${project.title} - Image ${index + 1}`}
-                        width={600}
-                        height={350}
-                        className="rounded-lg object-cover"
-                    />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <div className="w-full h-64 relative">
+            <Swiper
+                modules={[Navigation, Pagination, EffectFade, Autoplay]}
+                navigation
+                pagination={{ clickable: true }}
+                effect="fade"
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                loop={true}
+                className="w-full h-full"
+            >
+                {project.images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <Image
+                            src={image}
+                            alt={`${project.title} - Image ${index + 1}`}
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-lg"
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
 
-        <div className="p-6 flex justify-between items-center border-t border-gray-700">
-        <Link
-    href={project.github}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center bg-gray-800 text-gray-400 font-semibold px-4 py-2 rounded-full hover:text-white hover:bg-gray-700 transition-all duration-300 transform hover:scale-105 shadow-lg"
-  >
-    <BsGithub size={24} className="mr-2" />
-    View on GitHub
-  </Link>
-  
-  <Link
-    href={project.live}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center bg-accent text-black font-semibold px-4 py-2 rounded-full hover:from-green-500 hover:to-green-700 transition-all duration-300 transform hover:scale-105 shadow-lg"
-  >
-    {/* {<MdOutlineKeyboardDoubleArrowRight className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />} */}
-    Live Demo
-    <MdOutlineKeyboardDoubleArrowRight className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />
-  </Link>
-</div>
-
+        <div className="p-6 flex flex-col sm:flex-row justify-center items-center gap-4 border-t border-gray-700 w-full">
+            <Link
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center bg-gray-800 text-gray-400 font-semibold px-4 py-2 rounded-full hover:text-white hover:bg-gray-700 transition-all duration-300 transform hover:scale-105 shadow-lg"
+            >
+                <BsGithub size={24} className="mr-2" />
+                View on GitHub
+            </Link>
+            
+            <Link
+                href={project.live}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center bg-accent text-black font-semibold px-4 py-2 rounded-full hover:from-green-500 hover:to-green-700 transition-all duration-300 transform hover:scale-105 shadow-lg"
+            >
+                Live Demo
+                <MdOutlineKeyboardDoubleArrowRight className="ml-2 transition-transform duration-300 group-hover:translate-x-1" />
+            </Link>
+        </div>
     </motion.div>
 );
-
 
 const Work = () => {
     return (
